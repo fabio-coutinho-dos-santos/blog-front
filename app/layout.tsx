fcf0b1c7b136fa375778c762aaad0c2db5025b22
fabio-coutinho-dos-santos <fabio.santcou@gmail.com>
2026-05-30 10:28:33 -0300

    feat: disable ai chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { SearchProvider, SearchConfig } from 'pliny/search'
 import Header from '@/components/Header'
 import SectionContainer from '@/components/SectionContainer'
 import Footer from '@/components/Footer'
-import CareerChatWidget from '@/components/CareerChatWidget'
+// import CareerChatWidget from '@/components/CareerChatWidget'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
@@ -102,7 +102,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
               <Header />
               <main className="mb-auto">{children}</main>
-              <CareerChatWidget />
+              {/* <CareerChatWidget /> */}
             </SearchProvider>
             <Footer />
           </SectionContainer>
